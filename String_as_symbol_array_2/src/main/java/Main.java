60954c845982317e01ee51f59a3d82e603d776ff
unknown <SiarheySkuratovich@yandex.ru>
2020-01-26 15:05:04 +0300

    exactly works

--- String_as_symbol_array_2/src/main/java/Main.java
+++ String_as_symbol_array_2/src/main/java/Main.java
@@ -1,31 +1,16 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        /////////////////123456789123456789123456789123456789
-        String string = "word blabla word and so on and words";
-        for (int i= 0; i < string.length(); i++) {
-            System.out.print(i);
-            System.out.print(string.charAt(i) + " ");
-        }
-        System.out.println();
-        String string2 = "letter blabla letter and so on and letter";
-        for (int i= 0; i < string2.length(); i++) {
-            System.out.print(i);
-            System.out.print(string2.charAt(i) + " ");
-        }
-        System.out.println();
-
-
-
+        String string = "word blabla word and wo and so on and words";
+        System.out.println(string);
         for (int i = 0; i < string.length() - 4; i++) {
-            System.out.print(i);
-            System.out.print(string.charAt(i) + " ");
             if (string.charAt(i) == 'w') {
                 i++;
                 if (string.charAt(i) == 'o') {
-                    if (string.charAt(i++) == 'r') {
-                        if (string.charAt(i++) == 'd') {
+                    i++;
+                    if (string.charAt(i) == 'r') {
+                        i++;
+                        if (string.charAt(i) == 'd') {
+                            i++;
                             string = string.substring(0, i - 4) + "letter" + string.substring(i);
                             i +=2;
                         }
@@ -33,7 +18,6 @@ public class Main {
                 }
             }
         }
-        System.out.println();
         System.out.println(string);
     }
 }
